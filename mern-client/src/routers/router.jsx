2d2204d7ea84a2d61b21bc6c7bc09d8ee3c6c5fc
nimishsara12 <nimishsarathe561@gmail.com>
2023-12-21 22:52:31 +0530

    front end raect app basic structure

---
+++ mern-client/src/routers/router.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hellnnnnnnnnnno world!</div>,
+    },
+  ]);
+  
+export default router;
